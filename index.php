cffb0390fb8d18607bb2b0eb1bffc6c5862bd082
PotateSlad <32211916+PotateSlad@users.noreply.github.com>
2017-10-02 11:03:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <!DOCTYPE html>
-
+<html>
+<title>Raccoon Siting Data Collection</title>
+<head>
+     <style>
+	 * {margin: 0; padding: 0;}
+	 body {background-color: #211F20; font-family: "Lucida Console", Monaco, monospace;}
+	 #container {margin: 0 auto; opacity: 0.8; text-align: center; width: 80%; margin-top: 100px;}
+	 #header {margin-left: 0; width: 100%; background-color: #B79FA6; color: #FFFFFF; 
+     border-radius: 5px; padding-top: 20px; opacity: 0.5; padding-bottom: 20px;}
+	 #infoBox {margin-right: 0; width: 64%; background-color: #8F787E; color: #FFFFFF; border-radius: 5px;
+	     padding-top: 20px; opacity: 0.5; float: right;}
+   #map {;}
+	 #footer {margin-left: 0; width: 100%; background-color: #8F787E; color: #FFFFFF; 
+	     border-radius: 5px; padding-top: 20px; opacity: 0.5; margin-top: 20px;}
+	 
+	 a {text-decoration: none; color: #FFFFFF; font-weight: bold; line-height: 125%;}
+	     a:hover {font-size: 125%; line-height: 100%;}
+	 h1 {font-size: 30px; line-height: 150%;}
+	 hr {width: 50%; border-style: none none dotted none; border-width: 6px; margin-left: 25%;}
+     </style>
+</head>
+<body>
+     <div id="container">
+	     <div id="header">
+       </div>
+       
+       <div id="infoBox">
+       </div>
+       
+       <div id="map">
+       </div>
+       
+       <div id="footer">
+       </div>
+     </div>
+</body>
 </html>
